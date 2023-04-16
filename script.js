3c60ca59ee962ebcf974cfc974f86d3c0c7357e2
Marciapan <122381666+Marciapan@users.noreply.github.com>
2023-04-16 23:14:18 +0200

    woop woop
    To do: failed scenario uitwerken
    laatste test met wachtwoord maken
    buttons mooi maken

--- script.js
+++ script.js
@@ -1,39 +1,35 @@
 console.log("Testing connection.... Link established.");
 
+var background = document.querySelector(".flexbox");
+
 var nextButton = document.querySelector("#knop1");
 var emailCheckButton = document.querySelector("#knop2");
 var finalButton = document.querySelector("#knop3");
-var emailField = document.querySelector("#text1");
+var emailField = document.querySelector("#field1");
+var passwordField = document.querySelector("#field2");
+
+var hackerPic = document.querySelector("hackerPic");
 
 //Tekst letter voor letter laten zien.
 var counter1 = 0;
 var counter2 = 0;
+var counter3 = 0;
 var txt1 = 'Hallo inspecteurs, ik heb een missie voor jullie.' +
-    ' Een hacker wilt de data van de hva stelen, en jullie moet dit voorkomen.' +
-    ' Kom achter het E-mailadres en het wachtwoord van de hacker, voordat hij zijn aanval loslaat.' +
+    ' Alleen via deze website is het veilig om te communiceren. '+
+    ' Een hacker wilt de data van de HvA stelen en wij moeten hem tegenhouden.' +
+    ' Vind het E-mailadres en het wachtwoord van de hacker, voordat hij zijn aanval loslaat. ' +
+    'Hints in deze koffer helpen jullie op weg. '+
     ' Na het doorgeven van het E-mailadres krijgen jullie van mij de volgorde van de nummers op het slot';
-var txt2 = 'Hallo inspecteurs, jullie hebben de koffer geopend. ' +
-    'Via deze site is het veilig om met mij te praten. ' +
+var txt2 = 'Hallo inspecteurs, jullie hebben het E-mailadres te pakken. ' +
     'We zijn nu op zoek naar het wachtwoord van de hacker die de hva gaat aanvallen. ' +
-    'De hints in deze koffer helpen je op weg. ' +
-    'Als je denkt het wachtwoord te hebben, kan je het aan ons doorgeven.';
-var speed = 50; /* The speed/duration of the effect in milliseconds */
-
-
-function showEmail() {
-    console.log("Volgende stap");
-    emailField.classList.remove("hidden");
-    emailCheckButton.classList.remove("hidden");
-}
-
-function checkAnswerEmail() {
-    if (emailField.value != "luciajonkhoff@outlook.nl") {
-        console.log("fail");
-    } else {
-        console.log("succes");
-        typeWriter2();
-    }
-}
+    'Als je denkt het wachtwoord te hebben, kan je het aan mij doorgeven.';
+var txt3 = 'Hallo inspecteurs, dank jullie wel voor het E-mailadres en het wachtwoord. '+
+            'Helaas is niets wat het lijkt. '+
+            'Jullie hebben mij de sleutel gegeven tot alle data van de HvA. '+
+            'Terwijl jullie bezig waren met alle puzzels, was ik bezig met het hacken van de beveiliging. '+
+            'Het enige dat ik nog nodig had was een ingang. '+
+            'Hartelijk bedankt.';
+var speed = 50; 
 
 function typeWriter() {
     if (counter1 < txt1.length) {
@@ -49,13 +45,54 @@ function typeWriter2() {
     if (counter2 < txt2.length) {
         document.getElementById("two").innerHTML += txt2.charAt(counter2);
         counter2++;
-        setTimeout(typeWriter, speed);
+        setTimeout(typeWriter2, speed);
+    
     } else {
         finalButton.classList.add("visible");
     }
 }
 
+function typeWriter3() {
+    if (counter3 < txt3.length) {
+        document.getElementById("three").innerHTML += txt3.charAt(counter3);
+        counter3++;
+        setTimeout(typeWriter3, speed);
+    
+    } else {
+        console.log("Congrats you failed the world.")
+    }
+}
+
+function showEmail() {
+    console.log("Volgende stap");
+    emailField.classList.remove("hidden");
+    emailCheckButton.classList.remove("hidden");
+}
+
+function checkAnswerEmail() {
+    if (emailField.value != "luciajonkhoff@outlook.nl") {
+        console.log("fail");
+        background.src = "../images/hacker.gif";
+        alert("Dit was een slecht idee, ik had andere mensen moeten gebruiken.");
+    } else {
+        console.log("succes");
+        typeWriter2();
+    }
+}
+//Goede nummer invoeren!!!!!!!!!!!!!!!!!!!!!!!!
+function checkAnswerPassword(){
+    if(passwordField.value == "1234"){
+        typeWriter3();
+        console.log("Succes");
+    } else {
+        console.log("Nope")
+;    }
+}
+
+
+
 typeWriter();
 
 nextButton.addEventListener("click", showEmail);
 emailCheckButton.addEventListener("click", checkAnswerEmail);
+finalButton.addEventListener("click", checkAnswerPassword);
