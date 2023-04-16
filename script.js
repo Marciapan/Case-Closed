b9749538a1c3c8f92662441f7fee10737888a3eb
Marciapan <122381666+Marciapan@users.noreply.github.com>
2023-04-16 11:33:10 +0200

    Add first documents
    added the first documents

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log ("test linking");
+
+var nextButton = document.querySelector("#knop1");
+var tekst2 = document.querySelector("#tekst2");
+var passwordButton = document.querySelector("#knop2");
+var tekst3 = document.querySelector("#tekst3");
+
+nextButton.addEventListener("click",test)
+passwordButton.addEventListener("click",password)
+
+function test(){
+    console.log("nextButton");
+    tekst2.classList.remove("hidden");
+    passwordButton.classList.remove("hidden");
+}
+
+function password(){
+    console.log("password correct")
+    tekst3.classList.remove("hidden");
+}
