ea3297506cdf61a271344e8310b430a7b1e4854d
Marciapan <122381666+Marciapan@users.noreply.github.com>
2023-04-16 17:00:30 +0200

    Begin gemaakt
    doorgaan met de 2e typewriter werkend krijgen... of vaker

--- script.js
+++ script.js
@@ -1,23 +1,61 @@
-console.log("test linking");
+console.log("Testing connection.... Link established.");
 
 var nextButton = document.querySelector("#knop1");
-var tekst2 = document.querySelector("#tekst2");
-var passwordButton = document.querySelector("#knop2");
-var tekst3 = document.querySelector("#tekst3");
+var emailCheckButton = document.querySelector("#knop2");
+var finalButton = document.querySelector("#knop3");
+var emailField = document.querySelector("#text1");
 
-nextButton.addEventListener("click", test)
-passwordButton.addEventListener("click", password)
+//Tekst letter voor letter laten zien.
+var counter1 = 0;
+var counter2 = 0;
+var txt1 = 'Hallo inspecteurs, ik heb een missie voor jullie.' +
+    ' Een hacker wilt de data van de hva stelen, en jullie moet dit voorkomen.' +
+    ' Kom achter het E-mailadres en het wachtwoord van de hacker, voordat hij zijn aanval loslaat.' +
+    ' Na het doorgeven van het E-mailadres krijgen jullie van mij de volgorde van de nummers op het slot';
+var txt2 = 'Hallo inspecteurs, jullie hebben de koffer geopend. ' +
+    'Via deze site is het veilig om met mij te praten. ' +
+    'We zijn nu op zoek naar het wachtwoord van de hacker die de hva gaat aanvallen. ' +
+    'De hints in deze koffer helpen je op weg. ' +
+    'Als je denkt het wachtwoord te hebben, kan je het aan ons doorgeven.';
+var speed = 50; /* The speed/duration of the effect in milliseconds */
 
-function test() {
-    console.log("nextButton");
-    tekst2.classList.remove("hidden");
-    passwordButton.classList.remove("hidden");
+
+function showEmail() {
+    console.log("Volgende stap");
+    emailField.classList.remove("hidden");
+    emailCheckButton.classList.remove("hidden");
 }
 
-function password() {
-    console.log("password correct")
-    tekst3.classList.remove("hidden");
+function checkAnswerEmail() {
+    if (emailField.value != "luciajonkhoff@outlook.nl") {
+        console.log("fail");
+    } else {
+        console.log("succes");
+        typeWriter2();
+    }
 }
 
+function typeWriter() {
+    if (counter1 < txt1.length) {
+        document.getElementById("one").innerHTML += txt1.charAt(counter1);
+        counter1++;
+        setTimeout(typeWriter, speed);
+    } else {
+        nextButton.classList.add("visible");
+    }
+}
 
+function typeWriter2() {
+    if (counter2 < txt2.length) {
+        document.getElementById("two").innerHTML += txt2.charAt(counter2);
+        counter2++;
+        setTimeout(typeWriter, speed);
+    } else {
+        finalButton.classList.add("visible");
+    }
+}
 
+typeWriter();
+
+nextButton.addEventListener("click", showEmail);
+emailCheckButton.addEventListener("click", checkAnswerEmail);
