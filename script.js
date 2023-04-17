acf61065bb33b3fc892ddcfc363baef95e28cd43
Marciapan <122381666+Marciapan@users.noreply.github.com>
2023-04-17 10:31:12 +0200

    final touches
    im pretty proud haha
    VERGEET NIET HET GOEDE WACHTWOORD NUMMER IN TE VOEREN ENZO HAHA

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ var counter1 = 0;
 var counter2 = 0;
 var counter3 = 0;
 var txt1 = 'Hallo inspecteurs, ik heb een missie voor jullie.' +
-    ' Alleen via deze website is het veilig om te communiceren. '+
+    // ' Alleen via deze website is het veilig om te communiceren. '+
     ' Een hacker wilt de data van de HvA stelen en wij moeten hem tegenhouden.' +
     ' Vind het E-mailadres en het wachtwoord van de hacker, voordat hij zijn aanval loslaat. ' +
     'Hints in deze koffer helpen jullie op weg. '+
@@ -48,6 +48,7 @@ function typeWriter2() {
         setTimeout(typeWriter2, speed);
     
     } else {
+        passwordField.classList.remove("hidden");
         finalButton.classList.add("visible");
     }
 }
@@ -72,20 +73,25 @@ function showEmail() {
 function checkAnswerEmail() {
     if (emailField.value != "luciajonkhoff@outlook.nl") {
         console.log("fail");
-        background.src = "../images/hacker.gif";
+        background.classList.add("hackerPic");
         alert("Dit was een slecht idee, ik had andere mensen moeten gebruiken.");
     } else {
         console.log("succes");
+        background.classList.remove("hackerPic");
         typeWriter2();
     }
 }
 //Goede nummer invoeren!!!!!!!!!!!!!!!!!!!!!!!!
 function checkAnswerPassword(){
     if(passwordField.value == "1234"){
+        background.classList.remove("hackerPic");
         typeWriter3();
         console.log("Succes");
+        background.classList.add("hackerPic");
     } else {
-        console.log("Nope")
+        console.log("fail");
+        background.classList.add("hackerPic");
+        alert("Misschien moet ik toch maar zelf hack- ach ik geef ze nog iets meer tijd.");
 ;    }
 }
 
